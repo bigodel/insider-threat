f63f7d8f17e3fd8c4212d6ae60bcfc869bda37db
João Pedro <jpedrodeamorim@gmail.com>
2018-11-27 08:01:49 -0300

    Removed whitespace and comments

--- Tree.java
+++ Tree.java
@@ -1,6 +1,6 @@
 public class Tree {
-    
-    
+
+
     private enum LogType {
         Http, Device, Login, LDAP;
     }
@@ -32,7 +32,7 @@ public class Tree {
             case LDAP:
             addLDAPLog((LDAPLog) log);
             break;
-            
+
             default:
             addCommonLog((CommonLog) log);
         }
@@ -47,12 +47,12 @@ public class Tree {
                 UserField userNode = (UserField) node;
                 if(userNode.getUser_id() != null &&
                     userNode.getUser_id().contains(log.getUser_id())){
-                   
+
                     user = userNode;
                 }
             }
         }
-    
+
         if(user == null){
             user = new UserField(log);
             root.children.add(user);
@@ -66,7 +66,7 @@ public class Tree {
     private void addCommonLog(CommonLog log)
     {
         UserField user = null;
-        
+
         if (!root.children.isEmpty()){
             for(Node node : root.children){
                 UserField userNode = (UserField) node;
@@ -75,12 +75,12 @@ public class Tree {
                 }
             }
         }
-        
+
         if(user == null){
             user = new UserField(log.getUser());
             root.children.add(user);
         }
-        
+
         // this is a stub version, using a undefined timewindow
         TimeWindow timeWindow = null;
         if (user.children.size() == 0){
@@ -90,7 +90,7 @@ public class Tree {
         else {
             timeWindow = (TimeWindow) user.children.get(0);
         }
-        
+
         Computer pc = null;
         for(Node node : timeWindow.children){
             Computer computerNode = (Computer) node;
@@ -98,43 +98,43 @@ public class Tree {
                 pc = computerNode;
             }
         }
-        
+
         if(pc == null)
         {
             pc = new Computer(log.getPc());
             timeWindow.children.add(pc);
         }
-        
+
         Activity activity = null;
         if(pc.children.size() == 0){
             activity = new Activity();
             pc.children.add(activity);
         }
-        
+
         else{
             activity = (Activity) pc.children.get(0);
         }
-        
+
         Action action;
         boolean succesfullInsertion = false;
-        
+
         switch(currentLogType){
-            
-            case Http:
-                action = new Url(/*log.id, */log.getActivity(), log.getDate());
-                if(activity.addAction(action)){
-                    succesfullInsertion = true;
-                }
+
+        case Http:
+            action = new Url(log.getActivity(), log.getDate());
+            if(activity.addAction(action)){
+                succesfullInsertion = true;
+            }
                 break;
             case Device:
-                
-                action = new Usb(/*log.id, */ log.getActivity(), log.getDate());
+
+                action = new Usb(log.getActivity(), log.getDate());
                 if(activity.addAction(action)){
                     succesfullInsertion = true;
                 }
                 break;
             case Login:
-                action = new Login(/*log.id, */ log.getActivity(), log.getDate());
+                action = new Login(log.getActivity(), log.getDate());
                 if(activity.addAction(action)){
                     succesfullInsertion = true;
                 }
@@ -158,8 +158,8 @@ public class Tree {
         }
         return string.toString();
     }
-   /* 
-    private final class ActivityIndexes{
+   /*
+     private final class ActivityIndexes{
         public static final int Http = 0;
         public static final int USB = 1;
         public static final int LOGIN = 2;
